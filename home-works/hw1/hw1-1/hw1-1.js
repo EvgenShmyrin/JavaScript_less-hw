492440105fbc55a5106f7fad3e7798bf12ab58be
EvgenShmyrin <shmirin@gmail.com>
2025-04-18 19:19:09 +0300

    hw1-1

---
+++ home-works/hw1/hw1-1/hw1-1.js
@@ -0,0 +1,44 @@
+// Створити змінні. Присвоїти кожному з них значення: ‘hello’,’owu’,’com’, ‘ua’, 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//
+//     Вивести кожну змінну за допомогою: console.log
+let a;
+let b;
+let c;
+let d;
+let e;
+let f;
+let g;
+let h;
+let i;
+let j;
+let k;
+let l;
+let m;
+a = 'hello';
+b = 'owu';
+c = 'com';
+d = 'ua';
+e = 1;
+f = 10;
+g = -999;
+h = 123;
+i = 3.14;
+j = 2.7;
+k = 16;
+l = true;
+m = false;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(i)
+console.log(j)
+console.log(k)
+console.log(m)
+console.log(l)
+
+
