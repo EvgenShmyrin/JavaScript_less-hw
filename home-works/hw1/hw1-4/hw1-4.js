42d06ce1104196174c51bcc6619b6c7cdfd7e9e8
EvgenShmyrin <shmirin@gmail.com>
2025-04-19 17:55:50 +0300

    hw1-4

--- home-works/hw1/hw1-4/hw1-4.js
+++ home-works/hw1/hw1-4/hw1-4.js
@@ -1,8 +1,7 @@
 // – За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
 
-const middle_Name = prompt('Введіть ваше ім\'я:');
-const last_name = prompt('Введіть ваше по-батькові:');
-const age = prompt('Введіть скільки вам років:');
+let middle_Name = prompt('Введіть ваше ім\'я:');
+let last_name = prompt('Введіть ваше по-батькові:');
+let age = prompt('Введіть скільки вам років:');
 
-// alert(`Привіт, ${middle_Name} ${last_name}. Тобі ${age} роки`);
 console.log(`Привіт, ${middle_Name} ${last_name}. Тобі ${age} роки`);
