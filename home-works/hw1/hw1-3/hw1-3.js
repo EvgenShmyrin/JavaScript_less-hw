5edb9558183102db03cd28737cd45ffa7b18d6e3
EvgenShmyrin <shmirin@gmail.com>
2025-04-19 17:16:44 +0300

    hw1-3

---
+++ home-works/hw1/hw1-3/hw1-3.js
@@ -0,0 +1,11 @@
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = ‘100’;
+// let c = true;
+let a=100;
+let b='100';
+let c=true
+
+console.log('a -', typeof a);
+console.log('b -', typeof b);
+console.log('c -', typeof c);
