8f2283d4d65de2266f7345ade472385f48918f03
EvgenShmyrin <shmirin@gmail.com>
2025-04-19 17:06:23 +0300

    hw1-2

---
+++ home-works/hw2/hw1-1/hw1-2.js
@@ -0,0 +1,12 @@
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм
+// ПІБ.З’єднати їх в одну змінну person (Не об’єкт, просто за допомогою конкатенації)
+let firstName;
+let lastName;
+let middleName;
+
+firstName = 'Шмирін';
+middleName = 'Євгеній';
+lastName = 'Олександрович';
+
+// console.log(firstName + " " + middleName + " " + lastName);
+console.log(`${firstName} ${middleName} ${lastName}`);
