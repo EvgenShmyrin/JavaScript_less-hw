3e2ef4e8236e2f871e80de35b98a69d8402d110b
EvgenShmyrin <shmirin@gmail.com>
2025-04-21 12:02:24 +0300

    hw2-2

---
+++ home-works/hw2/hw2-2/hw2.js
@@ -0,0 +1,9 @@
+// Створити 3 об’єкти які описують книги. Поля об’єкту : title, pageCount, genre.
+
+let book1
+let book2
+let book3
+
+book1 = {title:'War and peace', pageCount:1300, genre:'Novel'};
+book2 = {title:'Crime and Punishment', pageCount:608, genre:'Drama'};
+book3 = {title:'Mind hacking', pageCount:450, genre:'Self-development'};
