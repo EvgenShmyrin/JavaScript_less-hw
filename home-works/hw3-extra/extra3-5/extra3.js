bdb9b50bfa887f18196cb090ca969c39a98d7a84
EvgenShmyrin <shmirin@gmail.com>
2025-04-26 14:09:39 +0300

    extra3-5

--- home-works/hw3-extra/extra3-5/extra3.js
+++ home-works/hw3-extra/extra3-5/extra3.js
@@ -1,55 +1,85 @@
 // #reLkOkTB29Q
-// створити масив книжок (назва, кількість сторінок, автори , жанри).
+// створити масив книжок (назва, кількість сторінок, автори, жанри).
+
+let arr = [
+    {
+        title: "Майстер і Маргарита",
+        pages: 480,
+        authors: ["Михайло Булгаков"],
+        genres: ["Роман", "Містика", "Фантастика"]
+    },
+    {
+        title: "Тарас Бульба",
+        pages: 320,
+        authors: ["Микола Гоголь"],
+        genres: ["Пригоди", "Історичний роман"]
+    },
+    {
+        title: "Сто років самотності",
+        pages: 417,
+        authors: ["Габріель Гарсія Маркес", "Angey Duda"],
+        genres: ["Магічний реалізм", "Роман"]
+    },
+    {
+        title: "Гаррі Поттер і філософський камінь",
+        pages: 350,
+        authors: ["Джоан Ролінг"],
+        genres: ["Фентезі", "Пригоди"]
+    }
+];
+
+// – знайти найбільшу книжку.
+
+// let long_page=arr[0].pages
+// let long_book=[]
+// for (let key of arr) {
+//     if (key.pages >= long_page) {
+//         long_page = key.pages
+//         long_book.push(key)
+//     }
+// }console.log(`Найбільша книга має ${long_page} сторінок і називається "${long_book[0].title}" `)
 
 // – знайти книжку/ки з найбільшою кількістю жанрів
-// – знайти книжку/ки з найдовшою назвою
-// – знайти книжку/ки, які писали 2 автори
-// – знайти книжку/ки, які писав 1 автор
-let arr=[
-    {
-        name: "Adventure in future",
-        page: 800,
-        author: {
-            number1: 'Nic Alex',
-            number2: 'Forex Roy',
-        },
-        genre: {
-            one: 'fantasy',
-            two: 'thriller',
-            three: 'romance',
-            four: 'adventure',
-        }
-    },{
-        name: "Var and peace",
-        page: 1000,
-        author: {
-            number1: 'Lev Tolstoy',
-        },
-        genre: {
-            two: 'var',
-            three: 'romance',
-        }
-    }
-]
-// – знайти найбільшу книжку.
-// let long_page=[]
-for (let key of arr ) {
-    // console.log(long_page.length);
-    let long_page=[]
-    if (long_page.length === 1) {
-        if (long_page[0] < long_page[1]) {
-        long_page[0] = key.page;
-        console.log(long_page + 'one');
 
-    }else{
-        long_page[0] = key.page;
-        console.log(long_page + 'zerro');
-        }
-    }console.log(long_page.length+'last');
-}
-//
-// else if (long_page.length === 1) {
-//     if (long_page[0] < long_page[1]) {
-//         long_page[0] = key.page;
-//         console.log(long_page + 'one');
+// let long_genre=0
+// let long_book=[]
+// for (let key of arr) {
+//     if (key.genres.length >= long_genre) {
+//         long_genre = key.genres
+//         long_book.push(key.title)
 //     }
+// }console.log(`Книга "${long_book[0]}" має найбільшу кількість жанрів - ${long_genre.length} шт `)
+
+// – знайти книжку/ки з найдовшою назвою
+
+// let long_title=0
+// let long_book=[]
+// for (let key of arr) {
+//     if (key.title.length >= long_title) {
+//         long_title = key.title.length;
+//         long_book.push(key.title)
+//     }
+// }console.log(`Книга "${long_book[0]}" має найдовшу назву - ${long_title} букв `)
+
+// – знайти книжку/ки, які писали 2 автори
+
+// let twu_authors=1
+// let authors_book=[]
+// for (let key of arr) {
+//     if (key.authors.length > twu_authors) {
+//         twu_authors = key.authors.length;
+//         authors_book.push(key.title)
+//     }
+// }console.log(`Книга "${authors_book[0]}" має ${twu_authors} авторів `)
+
+ // – знайти книжку/ки, які писав 1 автор
+//
+// let one_authors =0;
+// let book=[];
+// for (let key of arr) {
+//     if (key.authors.length === 1) {
+//         one_authors++;
+//         book.push(key.title)
+//
+//     }
+// }console.log(`${one_authors} книги: "${book}" мають по одному автору.`)
